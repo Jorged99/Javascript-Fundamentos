1f2c884bc027050edc7f879faac554f3b2ea8957
Jorged99 <jorgeald724@gmail.com>
2025-02-24 21:30:41 -0600

    Update variables.js

---
+++ Javascript/variables.js
@@ -0,0 +1,9 @@
+// Usando var (no recomendado en ES6+)
+var nombre = "Juan";
+console.log(nombre); // Output: Juan
+
+// Usando let (permite reasignación)
+let edad = 25;
+console.log(edad); // Output: 25
+edad = 26;
+console.log(edad); // Output: 26 
